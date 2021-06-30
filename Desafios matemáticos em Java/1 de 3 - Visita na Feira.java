fef381a3f34377c14425b0111d16ed10be834ce7
Jhony Smith <61160773+JhonySmithSilva@users.noreply.github.com>
2021-06-29 23:08:54 -0300

    algumas modificações no arquivo

--- Desafios matemáticos em Java/1 de 3 - Visita na Feira.java
+++ Desafios matemáticos em Java/1 de 3 - Visita na Feira.java
@@ -1,5 +1,5 @@
 /*
-					Desafio
+			Desafio 
 				  
 	Você está na feira com a sua sacola e parou em uma banca. 
 O feirante lhe entregou pimentões amarelos e vermelhos. 
@@ -12,17 +12,17 @@ seu resultado na variável X (total de pimentões).
 Apresente X como descrito na mensagem de exemplo abaixo. 
 Não apresente outra mensagem além da mensagem especificada.
 
-					Entrada
+			Entrada
 A entrada contém 2 valores inteiros, separados por um espaço.
 
-					Saída
+			Saída
 Imprimir a mensagem "X = " (sendo a letra X maiúscula) 
 seguido pelo valor da variável X e pelo final de linha. 
 Assegure que exista um espaço antes e depois do sinal de igualdade.
 
    Exemplos de Entrada	             Exemplos de Saída
    
-           11 7                           X = 18
+           11 7                           X = 18 
 
           -11 6                           X = -5
 
